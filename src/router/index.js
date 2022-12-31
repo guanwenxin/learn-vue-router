b05cf47f8377355899a091c10f4352ea6d77770b
guanwenxin <764434043@qq.com>
2022-12-31 15:59:21 +0800

    vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routesArr = [
   {
     path: '/',
-    name: 'home',
+    name: 'home', // 随意给
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/main',
+    name: 'main',
+    // 路由懒加载
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: MainView
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes: routesArr
 })
 
 export default router
