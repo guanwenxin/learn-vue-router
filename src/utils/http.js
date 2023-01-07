3fa93d77c1882369f1dd4b0f235fdee27964951a
guanwenxin <764434043@qq.com>
2023-01-07 17:12:35 +0800

    天气api

--- src/utils/http.js
+++ src/utils/http.js
@@ -8,7 +8,7 @@ const http = axios.create({
     baseURL: PRODUCTINO_URL,
     timeout: 1000,
     headers: {
-        Authorization: 'APPCODE 51666496883a48b2b4e371dcb77903c0'
+        Authorization: 'APPCODE xxx'
     }
 })
 
