c7f4f70c9c63e3c8698b619e28a51092bce38f92
guanwenxin <764434043@qq.com>
2023-02-06 11:48:24 +0800

    jwt中间件

--- src/utils/http.js
+++ src/utils/http.js
@@ -8,9 +8,9 @@ const TEST_URL = 'http://127.0.0.1:3000/api'
 const http = axios.create({
     baseURL: TEST_URL,
     timeout: 1000,
-    headers: {
-        Authorization: 'APPCODE 51666496883a48b2b4e371dcb77903c0'
-    }
+    // headers: {
+    //     Authorization: 'APPCODE 51666496883a48b2b4e371dcb77903c0'
+    // }
 })
 
 export {
